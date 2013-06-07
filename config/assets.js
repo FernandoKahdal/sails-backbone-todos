01c39fa15e5daf73b2c616d10ca34e813045d2ce
Christopher Broome <ChrisBroome7@gmail.com>
2013-06-06 22:02:13 -0400

    Move libraries to public folder, rename assets

--- config/assets.js
+++ config/assets.js
@@ -5,9 +5,9 @@ module.exports.assets = {
 	// and then can be automatically injected in your layout/views via the view partials:
 	// ( assets.css(), assets.js() and assets.templateLibrary() )
 	sequence: [
-		'assets/mixins', 
-		'assets/js', 
-		'assets/styles', 
+		'assets/mixins',
+		'assets/js',
+		'assets/styles',
 		'assets/templates'
 	]
 };
